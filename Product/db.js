e472d74a415f1b4d6c1376b3e4817f4de0a39098
Rajshri Koshtgavkar <rKoshtgavkar@gmail.com>
2026-06-22 23:50:07 +0530

    Connect backend to Aiven MySQL

--- Product/db.js
+++ Product/db.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "product_browser",
-    waitForConnections: true,
-    connectionLimit: 10
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
